41d273bd57187ac1546e9113ad870835028329f0
shakilmrf8 <shakilmrf8@gmail.com>
2024-05-08 16:46:35 +0000

    added endpoints to urls.py , for CRUD operations

--- corporate_assets/corporate_assets/urls.py
+++ corporate_assets/corporate_assets/urls.py
@@ -15,8 +15,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from assets.views import *
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('user/', UserAPIView.as_view(), name='user'),
+    # jwt-authentication
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Company URLs
+    path('companies/', CompanyListCreateView.as_view(), name='company-list-create'),
+    path('companies/<int:pk>/', CompanyRetrieveUpdateDestroyView.as_view(), name='company-detail'),
+
+    # Device URLs
+    path('devices/', DeviceListCreateView.as_view(), name='device-list-create'),
+    path('devices/<int:pk>/', DeviceRetrieveUpdateDestroyView.as_view(), name='device-detail'),
+    path('devices/<int:pk>/assign/', DeviceAssignView.as_view(), name='device-assign'),
+
+    # Employee URLs
+    path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
+    path('employees/<int:pk>/', EmployeeRetrieveUpdateDestroyView.as_view(), name='employee-detail'),
+    path('employees/<int:pk>/assignments/', EmployeeAssignmentsListView.as_view(), name='employee-assignments-list'),
+    # DeviceAssignment URLs
+    path('device-assignments/', DeviceAssignmentListCreateView.as_view(), name='device-assignment-list-create'),
+    path('device-assignments/<int:pk>/', DeviceAssignmentRetrieveUpdateDestroyView.as_view(), name='device-assignment-detail'),
+
+    # for admin
+    path("admin/", admin.site.urls),
+
+    # for api authentications
+    path('api-auth/', include('rest_framework.urls')),
+
+    # for swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
