c78a01783d0be71fd6c9cda94e5f8c7a0b5bcc08
shakilmrf8 <shakilmrf8@gmail.com>
2024-05-08 16:27:56 +0000

    - added views.py (includes jwt, user auth, models retrieve ,list etc)  - installed djangorestframework-simplejwt

--- corporate_assets/assets/views.py
+++ corporate_assets/assets/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from.models import Company, Employee, Device, DeviceAssignment, DeviceConditionLog
+from.serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer, DeviceAssignmentSerializer, DeviceConditionLogSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
+
+class UserAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class CompanyListCreateView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated]
+
+class CompanyRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
+
+class EmployeeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class DeviceListCreateView(generics.ListCreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeviceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class DeviceAssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = DeviceAssignment.objects.all()
+    serializer_class = DeviceAssignmentSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeviceAssignmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeviceAssignment.objects.all()
+    serializer_class = DeviceAssignmentSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class DeviceConditionLogListCreateView(generics.ListCreateAPIView):
+    queryset = DeviceConditionLog.objects.all()
+    serializer_class = DeviceConditionLogSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeviceConditionLogRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeviceConditionLog.objects.all()
+    serializer_class = DeviceConditionLogSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class ObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (IsAuthenticated,)
+
+class RefreshTokenView(TokenRefreshView):
+    permission_classes = (IsAuthenticated,)
